1715b6d04f5e601917b4d307d4571caf9d792495
Avch Greg <avchargrisha@gmail.com>
2025-01-05 20:36:36 +0100

    feat(consulting): add API call for consulting window

--- src/app/feature/landing/components/landing-consulting/landing-consulting.component.ts
+++ src/app/feature/landing/components/landing-consulting/landing-consulting.component.ts
@@ -1,34 +1,120 @@
+import { ConsultingService } from './../../services/consulting.service';
 import { NgIf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import {ReactiveFormsModule,FormsModule, FormBuilder, FormGroup, Validators} from '@angular/forms'
+import {
+  ReactiveFormsModule,
+  FormsModule,
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl,
+} from '@angular/forms';
+import { ConsultingModel } from '../../models/consulting.model';
+import { catchError, of } from 'rxjs';
+import { MessageModel } from 'src/app/shared/models/message-model';
 
 @Component({
   selector: 'app-landing-consulting',
   templateUrl: './landing-consulting.component.html',
   styleUrls: ['./landing-consulting.component.scss'],
-  imports: [ReactiveFormsModule,FormsModule, NgIf]
+  imports: [ReactiveFormsModule, FormsModule, NgIf],
 })
 export class LandingConsultingComponent implements OnInit {
-
   contactForm!: FormGroup;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private consultingService: ConsultingService) {}
 
   ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  private initializeForm(): void {
     this.contactForm = this.fb.group({
-      name: ['', Validators.required],
-      phone: ['', Validators.required],
-      email: ['', [Validators.email]],
+      customer: this.fb.group({
+        name: ['', Validators.required],
+        phone: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+      }),
       comment: [''],
-      contactByMessenger: [false]
+      contactMe: [false],
     });
   }
 
+  get nameControl(): FormControl {
+    return this.getControl('customer.name');
+  }
+  
+  get phoneControl(): FormControl {
+    return this.getControl('customer.phone');
+  }
+  
+  get emailControl(): FormControl {
+    return this.getControl('customer.email');
+  }
+  
+  get commentControl(): FormControl {
+    return this.getControl('comment');
+  }
+  
+  get contactMeControl(): FormControl {
+    return this.getControl('contactMe');
+  }
+
   onSubmit(): void {
-    if (this.contactForm.valid) {
-      this.contactForm.reset();
-    } else {
+    if (this.contactForm.invalid) {
       this.contactForm.markAllAsTouched();
+      return;
     }
+
+    const formValue = this.contactForm.value;
+    const consultingModel = ConsultingModel.fromJson({ ...formValue });
+
+    this.createConsultingRequest(consultingModel);
+  }
+
+  private createConsultingRequest(consultingModel: ConsultingModel): void {
+    this.consultingService
+      .createConsultingRequest(consultingModel)
+      .pipe(
+        catchError((errorResponse) => {
+          this.handleError(errorResponse);
+          return of(null);
+        })
+      )
+      .subscribe((response) => {
+        if (response) {
+          this.handleSuccess(response);
+          this.resetForm();
+        }
+      });
+  }
+
+  private handleSuccess(consulting: ConsultingModel): void {
+    // TODO: Add success message
+  }
+
+  private handleError(errorResponse: any): void {
+    const error = MessageModel.fromJson(errorResponse);
+    // TODO: Add failure message
+  }
+
+  private resetForm(): void {
+    this.contactForm.reset({
+      customer: {
+        name: '',
+        phone: '',
+        email: '',
+      },
+      comment: '',
+      contactMe: false,
+    });
+  }
+  
+  private getControl(path: string): FormControl {
+    const control = this.contactForm.get(path);
+    if (!control) {
+      throw new Error(`Control with path "${path}" not found in the form group.`);
+    }
+    return control as FormControl;
   }
 }
