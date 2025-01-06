ad75ebeb0ad14fe3ed3bef9d413363d7da80e42a
Avch Greg <avchargrisha@gmail.com>
2025-01-06 18:14:35 +0100

    feat(landing): category card cross button

--- src/app/feature/products/services/mocks/category-mock.service.ts
+++ src/app/feature/products/services/mocks/category-mock.service.ts
@@ -4,7 +4,7 @@ import { CategoryModel } from '../../../products/models/category-model';
 import { ICategoryService } from '../interfaces/category.service.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CategoryMockService implements ICategoryService {
   private mockCategories: CategoryModel[] = [
@@ -12,19 +12,20 @@ export class CategoryMockService implements ICategoryService {
       id: 1,
       name: 'Інвертори',
       image: '/assets/images/category-1-Photoroom.png',
-      description: 'I Love KIRILL'
+      description: 'I Love KIRILL',
     } as CategoryModel,
     {
       id: 2,
       name: 'Акумулятори',
       image: '/assets/images/category-2-Photoroom.png',
-      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore vero quisquam magni delectus, voluptatem quia aliquam praesentium accusantium aperiam dicta totam consectetur iusto doloremque laboriosam, nisi, suscipit eaque. Ab, dolores.'
-    }as CategoryModel,
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore vero quisquam magni delectus, voluptatem quia aliquam praesentium accusantium aperiam dicta totam consectetur iusto doloremque laboriosam, nisi, suscipit eaque. Ab, dolores.',
+    } as CategoryModel,
     {
       id: 9,
       name: 'Блок управління',
       image: '/assets/images/category-3-Photoroom.png',
-      description: 'Description for control units...'
+      description: 'Description for control units...',
     } as CategoryModel,
   ];
 
